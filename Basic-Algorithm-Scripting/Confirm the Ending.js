daf0d012ac3da2ac638a45bf6cfe0d009c3cd3d0
msmailcode <ms_code@outlook.com>
2016-10-21 15:30:40 +0800

    Create Confirm the Ending.js

---
+++ Basic-Algorithm-Scripting/Confirm the Ending.js
@@ -0,0 +1,5 @@
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  return target == str.substring(str.length - target.length, str.length);
+}
